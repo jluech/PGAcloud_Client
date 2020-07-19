ee8a9b527481d0053c451ab94e928a3dfa1ee45c
Janik Luechinger <janik.luechinger@uzh.ch>
2020-07-19 19:00:53 +0200

    refactor: rename docker networks
    refs #106

--- utilities/docker_utils.py
+++ utilities/docker_utils.py
@@ -3,7 +3,7 @@ import os
 import docker
 
 __docker_client = None
-__bridge_network = None
+__management_network = None
 
 
 def get_docker_client(cert_path, host_addr, host_port):
@@ -30,21 +30,21 @@ def get_docker_client(cert_path, host_addr, host_port):
     return __docker_client
 
 
-def get_bridge_network():
-    global __bridge_network
-    if __bridge_network:
-        return __bridge_network
+def get_management_network():
+    global __management_network
+    if __management_network:
+        return __management_network
     if not __docker_client:
         raise Exception("Create a docker client first, before creating a network...")
 
     # Creates a new docker network to bridge the manager to a connector.
-    __bridge_network = __docker_client.networks.create(
-        name="bridge-pga",
+    __management_network = __docker_client.networks.create(
+        name="pga-management",
         driver="overlay",
         check_duplicate=True,
         attachable=True,
         scope="swarm",
         labels={"PGAcloud": "PGA-Connection"},
     )
-    return __bridge_network
+    return __management_network
 
