70a2aa9b6c34bbee685258071aca27794076f503
Janik Luechinger <janik.luechinger@uzh.ch>
2020-07-19 16:48:13 +0200

    refactor: remove unused docker utils methods
    refs #106

--- utilities/docker_utils.py
+++ utilities/docker_utils.py
@@ -6,36 +6,6 @@ __docker_client = None
 __bridge_network = None
 
 
-def print_containers(list_all=False):
-    global __docker_client
-    if not __docker_client:
-        raise AttributeError("docker client not configured")
-
-    containers = __docker_client.containers.list(all=list_all)
-    for container in containers:
-        print(container.name)
-
-
-def print_image_history(image_tag):
-    global __docker_client
-    if not __docker_client:
-        raise AttributeError("docker client not configured")
-
-    images = __docker_client.images.list()
-    for image in images:
-        if image.tags.__contains__(image_tag):
-            for tag in image.tags:
-                print(tag)
-
-            history = image.history()
-            for entry in history:
-                for key in entry:
-                    print(key, " - ", entry[key])
-                print("")
-            print("")
-            print("")
-
-
 def get_docker_client(cert_path, host_addr, host_port):
     global __docker_client
     if __docker_client:
